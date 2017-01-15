cb13f234f398e26b65e6b87b02fac08c49aba31e
akhil <akhilkpdasan@gmail.com>
2017-01-15 09:32:17 +0530

    added search page and split homepage into a layout page(navbar and footer) also added a few product images.

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app.config['SECRET_KEY'] = "pefx86764asyuys23424rgwrhz2553462"
 #app.config['SERVER_NAME'] = "192.168.0.100:5000"
 
 # Open database connection
-db = MySQLdb.connect(host="localhost", user="root", passwd="aslk", db="ecommerce")
+#db = MySQLdb.connect(host="localhost", user="root", passwd="aslk", db="ecommerce")
+db = MySQLdb.connect(host="localhost", user="root", passwd="aslk", db="TESTDB2")
 cursor = db.cursor()
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-	if 'email_id' in session:      
-		return render_template('home.html',email_id=escape(session['email_id']))
-	return render_template('home.html',email_id='')
+	if 'username' in session:      
+		return render_template('home.html')
+	return render_template('home.html')
 
 
 @app.route('/login' , methods=['GET','POST'])
@@ -22,10 +23,18 @@ def login():
 	if request.method == 'POST':
 		email_id = request.form['InputEmail'] 
 		password = request.form['InputPassword']
-		sql = '''SELECT password FROM USERS WHERE email_id = '{}';'''.format(email_id)
+		sql = '''SELECT PASSWORD FROM USERS WHERE EMAIL_ID = '{}';'''.format(email_id)
 		cursor.execute(sql)
-		session['username'] = email_id
-		return redirect(url_for('home'))
+		db_password = cursor.fetchone()[0]
+		if db_password == password:
+			sql = '''SELECT NAME FROM USERS WHERE EMAIL_ID = '{}';'''.format(email_id)
+			cursor.execute(sql)
+			username = cursor.fetchone()[0]
+			print username
+			session['username'] = username
+			print "added to session"
+			return redirect(url_for('home'))
+	return "Something went wrong"
 
 
 
@@ -33,20 +42,46 @@ def login():
 @app.route('/register' , methods=['GET','POST'])
 def register():
 	if request.method == 'POST':
+		name = request.form['InputName']
 		email_id = request.form['InputEmail'] 
 		password1 = request.form['InputPassword1']
 		password2 = request.form['InputPassword2']
+		address = request.form['InputAddress']		
+		pin = int(request.form['InputPin'])
+		phone = int(request.form['InputPhone'])
 		if password1 == password2:
-			sql = ''' INSERT INTO USERS (email_id,password) VALUES ('{}','{}'); '''.format(email_id,password1)
+			sql = ''' INSERT INTO USERS (NAME,EMAIL_ID,PASSWORD,ADDRESS,PIN,PHONE) VALUES ('{}','{}','{}','{}','{}','{}'); '''.format(name,email_id,password1,address,pin,phone)
 			print sql
-			cursor.execute(sql)	    
+			cursor.execute(sql)
+			db.commit();	    
 			return redirect(url_for('home'))
+		else:
+			return "pass1 != pass2"
+		return "Something went wrong"
 
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
+
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+	cname = request.args.get('cname')
+	query = request.args.get('query')
+	if cname:
+		sql = ''' SELECT * FROM PRODUCTS WHERE CNAME = '{}' '''.format(cname.upper())
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		return render_template('search.html',products=data)
+	if query:
+		print query 
+		sql = '''SELECT * FROM PRODUCTS WHERE PNAME LIKE '%{0}%' OR CNAME LIKE '%{0}%' OR SUB_CATEGORY LIKE '%{0}%' OR BRAND LIKE '%{0}%';'''.format(query.upper())
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		return render_template('search.html',products=data)
+	return render_template('search.html',products=None)
 
 
 
